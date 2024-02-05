4d669702485f674f4c313b114818f3c7be0a0957
gglavanya <gglavanya06@gmail.com>
2024-02-04 21:13:14 -0800

    Update build

--- Python/scripts/build.sh
+++ Python/scripts/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 CURRENT_DIR=$(pwd) 
+
+#---Setup the venv----------
+rm -rf $CURRENT_DIR/testEnv # Remove any previous virtual envs
+python3 -m virtualenv $CURRENT_DIR/testEnv # Create new virtual env
 source $CURRENT_DIR/testEnv/bin/activate # Activate the test env
+python3 -m pip3 install pytest
+
+#-----Run the unit tests---------------
 cd $CURRENT_DIR/Python # Go to the appropriate directory
 pytest # Run the test
