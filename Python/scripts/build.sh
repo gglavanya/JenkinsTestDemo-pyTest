8ac14bba455dec0ab73ad7b7380082b14ce46349
gglavanya <gglavanya06@gmail.com>
2024-02-04 20:54:01 -0800

    Update the build script with comments

--- Python/scripts/build.sh
+++ Python/scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CURRENT_DIR=$(pwd)
-source $CURRENT_DIR/testEnv/bin/activate
-cd $CURRENT_DIR/Python
-pytest
+CURRENT_DIR=$(pwd) 
+source $CURRENT_DIR/testEnv/bin/activate # Activate the test env
+cd $CURRENT_DIR/Python # Go to the appropriate directory
+pytest # Run the test
