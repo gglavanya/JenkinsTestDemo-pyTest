a3ffadb277e2e838249405c9d2a78f52ecf4150b
gglavanya <gglavanya06@gmail.com>
2024-02-04 21:27:26 -0800

    Bug fix in build.sh

--- Python/scripts/build.sh
+++ Python/scripts/build.sh
@@ -5,7 +5,7 @@ CURRENT_DIR=$(pwd)
 rm -rf $CURRENT_DIR/testEnv # Remove any previous virtual envs
 python3 -m virtualenv $CURRENT_DIR/testEnv # Create new virtual env
 source $CURRENT_DIR/testEnv/bin/activate # Activate the test env
-python3 -m pip3 install pytest
+python3 -m pip install pytest
 
 #-----Run the unit tests---------------
 cd $CURRENT_DIR/Python # Go to the appropriate directory
